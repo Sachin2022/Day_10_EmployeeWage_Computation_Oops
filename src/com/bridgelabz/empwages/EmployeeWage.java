216470749b8d742d3085ded2882798060483d4cd
Textspot <sachinkatarki1995@gmail.com>
2022-01-14 22:03:19 +0530

    Added:Code to Check the Employee Wage for Multiple Companies

--- src/com/bridgelabz/empwages/EmployeeWage.java
+++ src/com/bridgelabz/empwages/EmployeeWage.java
@@ -4,22 +4,15 @@ public class EmployeeWage {
 
 	public static final int Is_Full_Time = 1;
 	public static final int Is_Part_Time = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int Num_Of_Working_Days = 20;
-	public static final int Max_Hrs_In_Month = 100;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
-		totalEmpWage();
-	}
-
-	static void totalEmpWage() {
+	public static int computeEmpWage(String company, int empRatePerHour, int noOfWorkingDays, int maxHoursPerMonth) {
 		int empHrs = 0;
 		int totalEmpHrs = 0;
 		int totalWorkingDays = 0;
-		int totalEmpWage = 0;
-
-		while (totalEmpHrs <= Max_Hrs_In_Month && totalWorkingDays < Num_Of_Working_Days) {
+		System.out.println("-----------------------------");
+		System.out.println("Company Name :" + company);
+		System.out.println("-----------------------------");
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < noOfWorkingDays) {
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
@@ -34,10 +27,19 @@ public class EmployeeWage {
 			}
 			totalEmpHrs += empHrs;
 
-			System.out.println("Day#: " + totalWorkingDays + "Employee Hr: " + empHrs);
+			System.out.println("Day :" + totalWorkingDays + "   Employee Hr: " + empHrs);
 		}
 
-		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("-----------------------------");
 		System.out.println("Total Employee Wage: " + totalEmpWage);
+		System.out.println("-----------------------------");
+		return totalEmpWage;
+	}
+
+	public static void main(String[] args) {
+
+		computeEmpWage("JIO", 40, 21, 120);
+		computeEmpWage("Wipro", 50, 24, 140);
 	}
 }
