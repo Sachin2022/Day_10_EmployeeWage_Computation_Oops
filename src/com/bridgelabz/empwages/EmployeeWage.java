7973a3358cad9fad33a4b17ca6e56e9f34ca6403
Textspot <sachinkatarki1995@gmail.com>
2022-01-14 20:59:50 +0530

    Added:Code to Check the Employee Daily Wage

--- src/com/bridgelabz/empwages/EmployeeWage.java
+++ src/com/bridgelabz/empwages/EmployeeWage.java
@@ -3,16 +3,20 @@ package com.bridgelabz.empwages;
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		attendanceCheck();
+		emopDailyWage();
 	}
 
-	static void attendanceCheck() {
-		int isFullTime = 1;
+	static void emopDailyWage() {
+		int Is_Full_Time = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
 		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-		if (empCheck == isFullTime)
-			System.out.println("Employee is present");
+		if (empCheck == Is_Full_Time)
+			empHrs = 8;
 		else
-			System.out.println("Employee is absent");
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee_wage:" + empWage);
 	}
-
 }
