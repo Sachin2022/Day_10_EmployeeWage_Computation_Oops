e9f4e994b6846cb79dde050d7202092d0c64d84a
Textspot <sachinkatarki1995@gmail.com>
2022-01-14 21:38:11 +0530

    Added:Code to Check the Employee Total Working Hours and Wage

--- src/com/bridgelabz/empwages/EmployeeWage.java
+++ src/com/bridgelabz/empwages/EmployeeWage.java
@@ -6,17 +6,20 @@ public class EmployeeWage {
 	public static final int Is_Part_Time = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int Num_Of_Working_Days = 20;
+	public static final int Max_Hrs_In_Month = 100;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		monthlyWage();
+		totalEmpWage();
 	}
 
-	static void monthlyWage() {
+	static void totalEmpWage() {
 		int empHrs = 0;
-		int empWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
 		int totalEmpWage = 0;
-		for (int day = 1; day <= Num_Of_Working_Days; day++) {
+		while (totalEmpHrs <= Max_Hrs_In_Month && totalWorkingDays < Num_Of_Working_Days) {
+			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case Is_Full_Time:
@@ -28,11 +31,12 @@ public class EmployeeWage {
 			default:
 				empHrs = 0;
 			}
-			empWage = empHrs * EMP_RATE_PER_HOUR;
-			totalEmpWage += empWage;
-			System.out.println("Employee_wage:" + empWage);
-		}
-		System.out.println("Total Employee_wage:" + totalEmpWage);
+			totalEmpHrs += empHrs;
 
+			System.out.println("Day#: " + totalWorkingDays + "Employee Hr: " + empHrs);
+		}
+
+		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Employee Wage: " + totalEmpWage);
 	}
 }
